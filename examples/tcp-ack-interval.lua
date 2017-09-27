0a18f6aeb8eb3a71cae5d246a083ac4bfc1cbea2
Jakub Sitnicki <jkbs@redhat.com>
2017-09-27 23:31:52 +0200

    examples: Update tcp-ack-interval to display a delay distribution
    ... instead of printing out interval samples.

--- examples/tcp-ack-interval.lua
+++ examples/tcp-ack-interval.lua
@@ -1,3 +1,17 @@
+--
+-- Measure time between ACKs and display the delay distribution.
+--
+-- TODO:
+-- * Extend to handle multiple flows per client. Requires a valid
+--   socket descriptor in _close() hoo (currently broken) to
+--   differentiate between histograms (one per flow).
+-- * Extend to handle multiple clients (threads). Requires either a
+--   synchronization mechanism between threads to serialize histogram
+--   printing, or a data passing mechanism between worker and main
+--   threads.
+-- * Get rid of open coded histogram generation. Introduce helpers.
+--
+
 client_socket(
   function (sockfd)
     assert(
@@ -6,6 +20,8 @@ client_socket(
                          SOF_TIMESTAMPING_TX_ACK,
                          SOF_TIMESTAMPING_OPT_TSONLY))
     )
+
+    hist = {}
   end
 )
 
@@ -19,13 +35,23 @@ client_recverr(
          cmsg.cmsg_type == SCM_TIMESTAMPING then
         local tss = scm_timestamping(cmsg.cmsg_data)
         local ts = tss.ts[0]
-        local usec = (ts.sec * 1000 * 1000)
-                   + (ts.nsec / 1000)
+        local ts_us = (ts.sec * 1000 * 1000)
+                    + (ts.nsec / 1000)
 
-        if usec_prev then
-          io.stderr:write(usec - usec_prev, "\n")
+        if prev_ts_us then
+          local ival_us = ts_us - prev_ts_us
+          local upper_us = 1
+          local i = 0
+
+          while ival_us >= upper_us do
+            upper_us = upper_us * 2
+            i = i + 1
+          end
+
+          hist[i] = (hist[i] or 0) + 1
         end
-        usec_prev = usec
+
+        prev_ts_us = ts_us
 
       elseif (cmsg.cmsg_level == SOL_IP and
               cmsg.cmsg_type == IP_RECVERR) or
@@ -36,5 +62,42 @@ client_recverr(
     end
 
     return n
+  end
+)
+
+client_close(
+  function (sockfd)
+    function bar(val, max, width)
+      if max == 0 then return "" end
+
+      local s = ""
+      local i = 0
+      while i < (width * val / max) do
+        s = s .. "="
+        i = i + 1
+      end
+      return s
+    end
+
+    local max = 0
+    for _, v in ipairs(hist) do
+      if v > max then max = v end
+    end
+
+    print()
+    print(string.format("%10s .. %-10s: %-10s |%-40s|",
+                        ">=", "< [us]", "Count", "Distribution"))
+    print()
+
+    local lower_us = 0
+    local upper_us = 1
+    for _, v in ipairs(hist) do
+      print(string.format("%10d -> %-10d: %-10d |%-40s|",
+                          lower_us, upper_us, v, bar(v, max, 40)))
+      lower_us = upper_us
+      upper_us = upper_us * 2
+    end
+
+    print()
   end
 )
