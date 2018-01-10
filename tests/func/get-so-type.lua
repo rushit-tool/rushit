1860b0d2a3b3d784974bfca40e746fb30a1ea8ec
Jakub Sitnicki <jkbs@redhat.com>
2018-01-10 15:04:43 +0100

    tests: Remove obsolete functional test for setting socket option

--- tests/func/get-so-type.lua
+++
@@ -1,8 +0,0 @@
-function socket_hook()
-  local S = require('syscall')
-  local val, err = S.getsockopt(-1, S.c.SOL.SOCKET, S.c.SO.TYPE)
-  assert(not val and err.BADF)
-end
-
-client_socket(socket_hook)
-server_socket(socket_hook)
